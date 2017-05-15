c736296499c3642c9a9d20c156476df38ca6dd6d
Laura Woelbeling <s0548106@htw-berlin.de>
2017-05-15 12:45:47 +0200

    simplified leafj

--- src/main/java/Leaf_Classification.java
+++ src/main/java/Leaf_Classification.java
@@ -119,7 +119,7 @@ public class Leaf_Classification implements PlugInFilter {
         IJ.log( "Cropping..." );
         vorf.hide();
         bp.setRoi( bb );
-        vorf = new ImagePlus(imp.getShortTitle() + " (processed)", bp.crop());
+        //vorf = new ImagePlus(imp.getShortTitle() + " (processed)", bp.crop());
         vorf.show();
         
         imp.hide();
@@ -128,133 +128,51 @@ public class Leaf_Classification implements PlugInFilter {
         
         RoiManager rm = new RoiManager();
         rm = RoiManager.getInstance();
-        if (rm.getCount()>0) {
-            //if a ROI window is already open, this is necessary
-            rm.runCommand("Select All");
-            rm.runCommand("Delete");
-        }
+
         rm.add( imp, roi, 1 );
         rm.add( imp, bbroi, 2 );
         rm.add( imp, centerp, 3 );
         
         
-        // include LeafJ
-        ByteProcessor bp3 = cp.convertToByteProcessor();
-        //bp3.setAutoThreshold(ImageProcessor.ISODATA, ImageProcessor.OVER_UNDER_LUT );
-        bp3.setAutoThreshold("Moments", false, ImageProcessor.OVER_UNDER_LUT );
-        bp3.setRoi( bb );
-        ImagePlus temppi = new ImagePlus(imp.getShortTitle() + " (temporal)", bp3);
-        temppi.show();
-        //IJ.runPlugIn( temppi, "LeafJ", "" );
-        //LeafJ_ bla = new LeafJ_();
-        //bla.setup( "", temppi );
-        //bla.run(bp3);
-        findPetiole(bp3);
+        // find petiole
+        findPetiole(cp.convertToByteProcessor(), vorf);
     }
     
-    public void findPetiole(ImageProcessor ip) {
+    public void findPetiole(ImageProcessor tip, ImagePlus timp) {
         // copied from LeafJ
+        IJ.log( "start find petiole" );
         
-        double minThreshold = ip.getMinThreshold();
-        double maxThreshold = ip.getMaxThreshold();
-        //sampleDescription sd = new sampleDescription();
-        //LeafResults results = new LeafResults(imp.getShortTitle() + "_measurements.txt");
+        tip.setAutoThreshold(ImageProcessor.ISODATA, ImageProcessor.OVER_UNDER_LUT );
+        tip.setAutoThreshold("Moments", false, ImageProcessor.OVER_UNDER_LUT );
 
-        
-        if (minThreshold == ImageProcessor.NO_THRESHOLD) {
-            IJ.showMessage("A thresholded image is required.");
-            return;
-        }
-        
-        //sd.setDescription(imp.getTitle()); //get user input for sample description and add file name    
-        
-        ImagePlus timp = new ImagePlus("temporary",ip.crop()); //temporary ImagePlus to hold current ROI
         Calibration cal = timp.getCalibration();
-        ImageProcessor tip = timp.getProcessor(); //ImageProcessor associated with timp
-        tip.setThreshold(minThreshold,maxThreshold,ImageProcessor.NO_LUT_UPDATE);
         ResultsTable rt = new ResultsTable();
-        //RoiManager rm = new RoiManager();
         RoiManager rm = RoiManager.getInstance();
-        if (rm.getCount()>0) {
-            //if a ROI window is already open, this is necessary
-            //rm.runCommand("Select All");
-            //rm.runCommand("Delete");
-        }
-            
-        double minParticleSize = 4000;
         
+        double minParticleSize = 4000;
         ParticleAnalyzer pa = new ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE
             //+ParticleAnalyzer.SHOW_RESULTS
             //+ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES
             ,Measurements.RECT+Measurements.ELLIPSE, rt, minParticleSize, Double.POSITIVE_INFINITY,.15,1);
-        
-        
-        IJ.log("Threshold: " + IJ.d2s(maxThreshold));
-    
         pa.analyze(timp,tip);
         
-        IJ.log("leaves found");
-        timp.show();        
-        timp.updateAndDraw();
+        leaf leafCurrent = new leaf();
+        leafCurrent.setLeaf( rt, 0, cal ); //set initial attributes
+        leafCurrent.scanLeaf(tip);          //do a scan across the length to determine widths
+        leafCurrent.findPetiole(tip);           //
         
-        //IJ.setColumnHeadings("Plant   Top Bottom");
-        
-        leaf[] leaves = new leaf[rt.getCounter()];  //an array of leaves
-        int[][] xpos = new int[rt.getCounter()][];  //an array to hold leaf position
-        Comparator<int[]> sorter = new Sort2D(1);   //to sort the xpos[][]
-
-        //need to sort leaves based on x position
-        //first go through the results table to get x position
-        //then sort before setting up leaves and processing
-        
-        //Find boundary x position for each leaf
-        for (int leafCurrent = 0; leafCurrent < rt.getCounter();leafCurrent++) {
-            xpos[leafCurrent] = new int[] {leafCurrent, (int) rt.getValue("BX",leafCurrent)};
-        }
-        
-        //sort the array based on x position
-        Arrays.sort(xpos,sorter);
-
-    
-        //set and process leaves
-        for (int i = 0; i < xpos.length;i++) {  //Move through the leaves
-            int leafCurrent = xpos[i][0];
-            IJ.log("leaf multi. i = " + IJ.d2s(i,0) + ". leaf current = " + IJ.d2s(leafCurrent,0));
-            IJ.log("setting leaf attributes for leaf: " + IJ.d2s(leafCurrent,0));
-            leaves[leafCurrent] = new leaf();
-            leaves[leafCurrent].setLeaf(rt, leafCurrent,cal);       //set initial attributes
-    
-            leaves[leafCurrent].scanLeaf(tip);          //do a scan across the length to determine widths
-            leaves[leafCurrent].findPetiole(tip);           //
+        //timp.updateAndDraw();
+        IJ.log("end find Petiole");
+        leafCurrent.addPetioleToManager(timp, tip, rm, 0);
+        leafCurrent.addBladeToManager(timp, tip, rm, 0);
             
-                timp.updateAndDraw();
-                IJ.log("end find Petiole");
-            
-            leaves[leafCurrent].addPetioleToManager(timp, tip, rm, i);
-            leaves[leafCurrent].addBladeToManager(timp, tip, rm, i);
-        
-        }//for leafCurrent
-        //timp = WindowManager.getCurrentImage();
-        timp.setProcessor("results",tip);
-        timp.setCalibration(imp.getCalibration());
-        ImageCanvas ic = new ImageCanvas(timp);
-        ic.setShowAllROIs(true);
-        timp.updateAndDraw();
-        
-        //allow user to make adjustments to ROI
-        WaitForUserDialog waitForAdjust = new WaitForUserDialog("Please adjust ROIs and press OK when done");
-        waitForAdjust.show();       
-        
-        //now I need to fill up a results table and include the sample description data.
-        //hmmm looks like results table can only take numeric values
-        //use TextPanel class
         
         /*if (sd.saveRois) rm.runCommand("Save", imp.getShortTitle() + sd.getTruncatedDescription() + "_roi.zip");
         results.setHeadings(sd.getFieldNames());
         results.show();
         results.addResults(sd,rm,tip,timp);*/
         
-        timp.close();
+        //timp.close();
     }
     
 
@@ -279,7 +197,7 @@ public class Leaf_Classification implements PlugInFilter {
 		new ImageJ();
 
 		// open the Clown sample
-		ImagePlus image = IJ.openImage("C:/Users/Laura/Dropbox/BA/Bilddatenbank/Laura/tilia_cordata/Tilia_cordata_5_MEW2014.png");
+		ImagePlus image = IJ.openImage("C:/Users/Laura/Dropbox/BA/Bilddatenbank/Laura/populus_tremula/Populus_tremula_20_MEW2014.png");
 		image.show();
 
 		// run the plugin
