2250bfee0c1ec1329bb61a06eed180890a59a668
Laura Woelbeling <s0548106@htw-berlin.de>
2017-06-07 10:55:19 +0200

    reincluded LeafJ

--- src/main/java/net/larla/leafy/common/LeafAnalyzer.java
+++ src/main/java/net/larla/leafy/common/LeafAnalyzer.java
@@ -10,21 +10,24 @@ import ij.*;
 import ij.gui.*;
 import ij.measure.*;
 import ij.plugin.filter.Analyzer;
+import ij.plugin.filter.ParticleAnalyzer;
+import ij.plugin.frame.RoiManager;
 import ij.process.ImageProcessor;
+import jnmaloof.leafj.leaf;
 import net.larla.leafy.datamodel.*;
 
 public class LeafAnalyzer {
-    
+
     public Leaf analyze(ImagePlus imp, ImagePlus mask, String groundTruth) {
-	
+
 	Leaf currentleaf = new Leaf(imp, imp.getShortTitle(), groundTruth, mask.getRoi(), mask);
 	runAnalyzer(currentleaf);
 	calcCCD(currentleaf);
 	fillResultsTable(currentleaf);
-	
+
 	return currentleaf;
     }
-    
+
     public void runAnalyzer(Leaf leaf) {
 	ResultsTable rt_temp = new ResultsTable();
 	double area, convexlength, perim, majoraxis, minoraxis, ellipsarea;
@@ -80,7 +83,7 @@ public class LeafAnalyzer {
 	leaf.setMincaliper(minoraxis);
 	leaf.setArea(area);
 	leaf.setPerimeter(perim);
-	
+
 	// use centroid as center point (average x and y of all pixels in region (center of mass requires grayscale pic)
 	leaf.setCentroidX(rt_temp.getValueAsDouble(rt_temp.getColumnIndex("X"), 0));
 	leaf.setCentroidY(rt_temp.getValueAsDouble(rt_temp.getColumnIndex("Y"), 0));
@@ -113,84 +116,84 @@ public class LeafAnalyzer {
     }
 
     public void calcCCD(Leaf leaf) {
-        // calculate ccd
+	// calculate ccd
 	double maxdist = 0;
-        double meandist = 0; 
-        double nmeandist = 0;
-        double vardist = 0;
-        double nvardist = 0;
-        double stdevdist, nstdevdist, har, har2;
+	double meandist = 0; 
+	double nmeandist = 0;
+	double vardist = 0;
+	double nvardist = 0;
+	double stdevdist, nstdevdist, har, har2;
 	Roi roi_leaf = leaf.getContour();
 	int pointcount = 0;
-        double dist, normdist;
-        //int contourpoints = roi_leaf.getPolygon().npoints;
-        Polygon polygon = roi_leaf.getPolygon();
-        double[] ccd = new double[polygon.npoints];
-        double[] normccd = new double[ccd.length];
-        for (int i = 0; i < ccd.length; i++) {
-        //for (Point p : roi_leaf) {
-            Point p = new Point(polygon.xpoints[i], polygon.ypoints[i]) ;
-            dist = Math.sqrt( Math.pow(p.getX() - leaf.getCentroidX(), 2) + Math.pow( p.getY() - leaf.getCentroidY(), 2 ) );
-            ccd[i] = dist;
-            maxdist = dist > maxdist ? dist : maxdist;
-            meandist += dist;
-            pointcount++;	// = regionpoints
-        }
-        meandist /= (double) pointcount;
-        // Varianz berechnen -> TODO: gleich oben mitberechnen
-        for (int i = 0; i < ccd.length; i++) {
-            vardist = vardist + Math.pow(ccd[i] - meandist, 2);
-            normdist = ccd[i] / maxdist;
-            normccd[i] = normdist;
-            nmeandist += normdist;
-        }
-        vardist /= (double) pointcount;
-        stdevdist = Math.sqrt( vardist );
-        nmeandist /= (double) pointcount;
-        
-        for (double nd : normccd) {
-            nvardist += Math.pow(nd - nmeandist, 2);
-        }
-        nvardist /= (double) pointcount;
-        nstdevdist = Math.sqrt( nvardist );
-        
-        har = meandist / stdevdist;
-        har2 = stdevdist / meandist;
-        
-        RadialDistances rd = new RadialDistances(ccd, maxdist, meandist, vardist, stdevdist, normccd, nmeandist, nstdevdist);
-        leaf.setCcd(rd);
-        leaf.setHaralick1(har);
-        leaf.setHaralick2(har2);
-        
+	double dist, normdist;
+	//int contourpoints = roi_leaf.getPolygon().npoints;
+	Polygon polygon = roi_leaf.getPolygon();
+	double[] ccd = new double[polygon.npoints];
+	double[] normccd = new double[ccd.length];
+	for (int i = 0; i < ccd.length; i++) {
+	    //for (Point p : roi_leaf) {
+	    Point p = new Point(polygon.xpoints[i], polygon.ypoints[i]) ;
+	    dist = Math.sqrt( Math.pow(p.getX() - leaf.getCentroidX(), 2) + Math.pow( p.getY() - leaf.getCentroidY(), 2 ) );
+	    ccd[i] = dist;
+	    maxdist = dist > maxdist ? dist : maxdist;
+	    meandist += dist;
+	    pointcount++;	// = regionpoints
+	}
+	meandist /= (double) pointcount;
+	// Varianz berechnen -> TODO: gleich oben mitberechnen
+	for (int i = 0; i < ccd.length; i++) {
+	    vardist = vardist + Math.pow(ccd[i] - meandist, 2);
+	    normdist = ccd[i] / maxdist;
+	    normccd[i] = normdist;
+	    nmeandist += normdist;
+	}
+	vardist /= (double) pointcount;
+	stdevdist = Math.sqrt( vardist );
+	nmeandist /= (double) pointcount;
+
+	for (double nd : normccd) {
+	    nvardist += Math.pow(nd - nmeandist, 2);
+	}
+	nvardist /= (double) pointcount;
+	nstdevdist = Math.sqrt( nvardist );
+
+	har = meandist / stdevdist;
+	har2 = stdevdist / meandist;
+
+	RadialDistances rd = new RadialDistances(ccd, maxdist, meandist, vardist, stdevdist, normccd, nmeandist, nstdevdist);
+	leaf.setCcd(rd);
+	leaf.setHaralick1(har);
+	leaf.setHaralick2(har2);
+
     }
 
-    
+
     public void fillResultsTable(Leaf leaf) {
-    	ResultsTable rt = ResultsTable.getResultsTable();
-    	rt.showRowNumbers( false );
-    	rt.incrementCounter();
-        //rt.addValue( "Label", leaf.getTitle() );
-    	if (leaf.getLeafclass() != "") {
-            rt.addValue( "Class", leaf.getLeafclass() );
-        } else {
-            rt.addValue( "Class", "?" );
-        }
-        rt.addValue( "Circularity", leaf.getCircularity() );
-        rt.addValue( "Roundness", leaf.getRoundness() );
-        rt.addValue( "Solidity", leaf.getSolidity() );
-        rt.addValue( "Convexity", leaf.getConvexity() );
-        rt.addValue( "Skewness", leaf.getSkewness() );
-        rt.addValue( "Kurtosis", leaf.getKurtosis() );
-        rt.addValue( "Elliptic", leaf.getElliptic() );
-        //results.addValue( "Haralick1", leaf.getHaralick1() );
-        rt.addValue( "Haralick2", leaf.getHaralick2());
-        rt.addValue( "nMeanDist", leaf.getCcd().getNormMean());
-        rt.addValue( "nDistSD", leaf.getCcd().getNormSdev());
-        
-            
+	ResultsTable rt = ResultsTable.getResultsTable();
+	rt.showRowNumbers( false );
+	rt.incrementCounter();
+	//rt.addValue( "Label", leaf.getTitle() );
+	if (leaf.getLeafclass() != "") {
+	    rt.addValue( "Class", leaf.getLeafclass() );
+	} else {
+	    rt.addValue( "Class", "?" );
+	}
+	rt.addValue( "Circularity", leaf.getCircularity() );
+	rt.addValue( "Roundness", leaf.getRoundness() );
+	rt.addValue( "Solidity", leaf.getSolidity() );
+	rt.addValue( "Convexity", leaf.getConvexity() );
+	rt.addValue( "Skewness", leaf.getSkewness() );
+	rt.addValue( "Kurtosis", leaf.getKurtosis() );
+	rt.addValue( "Elliptic", leaf.getElliptic() );
+	//results.addValue( "Haralick1", leaf.getHaralick1() );
+	rt.addValue( "Haralick2", leaf.getHaralick2());
+	rt.addValue( "nMeanDist", leaf.getCcd().getNormMean());
+	rt.addValue( "nDistSD", leaf.getCcd().getNormSdev());
+
+
 
     }
-    
+
     public ImagePlus getCCDplot(double[] distances, double max){
 	PlotWindow.noGridLines = false; // draw grid lines
 	double[] y = distances;
@@ -199,26 +202,26 @@ public class LeafAnalyzer {
 	    x[i] = 1.0 * i;
 	    i++;
 	}
-        Plot plot = new Plot("Contour Distances","Contour Point","Distance",x,y);
-        plot.setLimits(0,y.length, 0, max);
-        plot.setLineWidth(2);
-        plot.changeFont(new Font("Helvetica", Font.PLAIN, 16));
-        plot.setColor(Color.blue);
-        ImageProcessor ipp = plot.getProcessor();
-        //plot.show();
-        ImagePlus impp = new ImagePlus("Plot", ipp);
-        return impp;
-	
+	Plot plot = new Plot("Contour Distances","Contour Point","Distance",x,y);
+	plot.setLimits(0,y.length, 0, max);
+	plot.setLineWidth(2);
+	plot.changeFont(new Font("Helvetica", Font.PLAIN, 16));
+	plot.setColor(Color.blue);
+	ImageProcessor ipp = plot.getProcessor();
+	//plot.show();
+	ImagePlus impp = new ImagePlus("Plot", ipp);
+	return impp;
+
     }
-    
+
     public void saveCCDplot(Leaf leaf, String dir, String filename) {
 	ImagePlus impp = getCCDplot(leaf.getCcd().getNormccd(), 100d);
-	
+
 	WindowManager.setTempCurrentImage(impp);
 	// TODO: make directory, if not exist
-        IJ.save( dir + "ccd/" + filename + "-ccd.png" );
+	IJ.save( dir + "ccd/" + filename + "-ccd.png" );
     }
-    
+
     public void findLeafAxis(Leaf leaf, String dir, String filename) {
 	RadialDistances rd = leaf.getCcd();
 	double maxvalue = rd.getMaxdist();
@@ -227,7 +230,7 @@ public class LeafAnalyzer {
 	double[] nms = new double[points.length];
 	int section = points.length / 6;
 	int index;
-	
+
 	for (int i = 0; i < points.length; i++) {
 	    if (points[i] > threshold) {
 		for (int j = i - section; j < i + section; j++) {
@@ -235,7 +238,7 @@ public class LeafAnalyzer {
 		    index = index >= points.length ? index - points.length : index;	// positiver Überlauf
 		    if (points[index] > 0 && points[index] <= points[i]) {
 			points[index] = 0d;
-			
+
 		    }
 		}
 	    }
@@ -243,7 +246,55 @@ public class LeafAnalyzer {
 	ImagePlus impp = getCCDplot(points, maxvalue);
 	//WindowManager.setTempCurrentImage(impp);
 	// TODO: make directory, if not exist
-        //IJ.save( dir + "ccd/" + filename + "-ccdmax.png" );
+	//IJ.save( dir + "ccd/" + filename + "-ccdmax.png" );
 	impp.show();
+    }
+
+    public void findPetiole(ImagePlus imp_gray) {
+	// copied from LeafJ
+	IJ.log( "start find petiole" );
+	ImageProcessor tip = imp_gray.getProcessor();
+
+	tip.setAutoThreshold(ImageProcessor.ISODATA, ImageProcessor.OVER_UNDER_LUT );
+	//tip.setAutoThreshold("Moments", false, ImageProcessor.OVER_UNDER_LUT );
+
+	Calibration cal = imp_gray.getCalibration();
+	ResultsTable rt_tmp = new ResultsTable();
+	ResultsTable rt = ResultsTable.getResultsTable();
+	RoiManager rm = RoiManager.getInstance();
+
+	double minParticleSize = 4000;
+	ParticleAnalyzer pa = new ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE
+		+ParticleAnalyzer.SHOW_RESULTS
+		//+ParticleAnalyzer.EXCLUDE_EDGE_PARTICLES
+		,Measurements.RECT+Measurements.ELLIPSE, rt_tmp, minParticleSize, Double.POSITIVE_INFINITY,0,1);
+	pa.analyze(imp_gray,tip);   // TODO: nur Blatt messen, nicht alle Objekte im Bild
+
+
+	leaf leafCurrent = new leaf();
+	leafCurrent.setLeaf( rt_tmp, 0, cal ); //set initial attributes // TODO: nur aktuelle Ergebnisse hinzufügen
+	leafCurrent.scanLeaf(tip);          //do a scan across the length to determine widths
+	leafCurrent.findPetiole(tip);           //
+
+	//timp.updateAndDraw();
+	IJ.log("end find Petiole");
+	leafCurrent.addPetioleToManager(imp_gray, tip, rm, 4);
+	leafCurrent.addBladeToManager(imp_gray, tip, rm, 5);
+
+	/*if (sd.saveRois) rm.runCommand("Save", imp.getShortTitle() + sd.getTruncatedDescription() + "_roi.zip");
+        results.setHeadings(sd.getFieldNames());
+        results.show();
+        results.addResults(sd,rm,tip,timp);*/
+
+	ResultsTable rt_temp = new ResultsTable();
+	Analyzer petioleAnalyzer = new Analyzer(imp_gray,  Measurements.PERIMETER , rt_temp);
+	petioleAnalyzer.measure();
+	rt_temp.getValue("Perim.",rt_temp.getCounter()-1);
+	rt.addValue( "Petiole Length", rt_temp.getValue("Perim.",rt_temp.getCounter()-1) );
+	//rt.addValue( "Class", cls );
+
+	rt.show( "Results" );
+
+	//timp.close();
     }
 }
